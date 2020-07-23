7ef10d01a9af81e110e71d1fc0200e2b44983500
LKG <???>
2020-07-23 20:09:54 +0800

    完成提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <my-name></my-name>
+    <bottom-nav></bottom-nav>
+    <keep-alive>
+      <router-view />
+    </keep-alive>
   </div>
 </template>
+<script>
+import BottomNav from "@/components/common/BottomNav.vue";
+import MyName from "@/components/common/MyName.vue";
 
-<style lang="scss">
+export default {
+  components: {
+    BottomNav,
+    MyName,
+  },
+};
+</script>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  padding-top: 116px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,71 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
+const originalReplace = VueRouter.prototype.replace;
+VueRouter.prototype.replace = function replace(location) {
+  return originalReplace.call(this, location).catch((err) => err);
+};
+
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err);
+};
+
+const routes = [{
+    path: '/', //根路径
+    redirect: '/booklist'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/popular',
+    component: () => import('@/views/Popular/Popular.vue')
+  },
+  {
+    path: '/booklist',
+    component: () => import('@/views/Booklist/Booklist.vue'),
+    children: [{
+        path: '/',
+        component: () => import('@/views/Booklist/BooklistChild/BooklistChild.vue'),
+      },
+      {
+        path: 'search',
+        component: () => import('@/views/Booklist/BookSearch/BookSearch.vue'),
+      },
+      {
+        path: 'searching',
+        component: () => import('@/views/Booklist/Searching/Searching.vue'),
+      },
+      {
+        path: 'message',
+        component: () => import('@/views/Booklist/BookMessage/BookMessage.vue'),
+      }
+    ]
+  },
+  {
+    path: '/love',
+    component: () => import('@/views/Love/Love.vue'),
+    children: [{
+        path: '/',
+        component: () => import('@/views/Love/LoveChild/LoveChild.vue'),
+      },
+      {
+        path: 'mybook',
+        component: () => import('@/views/Love/MyBook/MyBook.vue'),
+      },
+      {
+        path: 'bookcontent',
+        component: () => import('@/views/Love/BookContent/BookContent.vue'),
+      }
+    ]
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  linkActiveClass: 'active'
 })
 
 export default router
